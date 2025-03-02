77464e9cfddf60480880cd97308c869161fd1180
Rosnaky <ronakjpatelr22@gmail.com>
2025-03-01 21:28:19 -0500

    responsive app bar

--- src/app/components/app_bar.tsx
+++ src/app/components/app_bar.tsx
@@ -9,19 +9,19 @@ const AppBar = () => {
                 alt="massdriver logo"
                 width={200}
                 height={200}
+                className="h-auto"
             />
-            <nav>
+            <nav className="hidden md:block">
                 <ul className="flex space-x-6">
-                <li><a href="#" className="hover:text-blue-600">Features</a></li>
-                <li><a href="#" className="hover:text-blue-600">Solutions</a></li>
-                <li><a href="#" className="hover:text-blue-600">Pricing</a></li>
-                <li><a href="#" className="hover:text-blue-600">Docs</a></li>
-                <li><a href="#" className="hover:text-blue-600">Templates</a></li>
-                <li><a href="#" className="hover:text-blue-600">Community</a></li>
+                    <li><a href="#" className="hover:text-blue-600">Features</a></li>
+                    <li><a href="#" className="hover:text-blue-600">Solutions</a></li>
+                    <li><a href="#" className="hover:text-blue-600">Pricing</a></li>
+                    <li><a href="#" className="hover:text-blue-600">Docs</a></li>
+                    <li><a href="#" className="hover:text-blue-600">Templates</a></li>
+                    <li><a href="#" className="hover:text-blue-600">Community</a></li>
                 </ul>
             </nav>
-            <div>
-
+            <div className="flex items-center space-x-4">
                 <button className="text-black">
                     <a 
                         href="https://app.massdriver.cloud/login" 
@@ -42,6 +42,14 @@ const AppBar = () => {
                         Try Free
                         <ArrowRightIcon className="pl-1 mt-0.5 h-5 w-5"/>
                     </a>
+                </button>
+            </div>
+
+            <div className="md:hidden flex items-center">
+                <button className="text-black" aria-label="Menu">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
                 </button>
             </div>
         </header>
